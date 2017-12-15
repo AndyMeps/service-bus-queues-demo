db71dcb4240b4805654218f085b1d125654eb30f
Andy Mepham <andymeps@yahoo.com>
2017-12-15 15:11:39 +0000

    Code file addition.

---
+++ service-bus-queues-demo/CreateMessages.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusQueuesDemo
+{
+    public static class CreateMessages
+    {
+        const int numberOfMessages = 10;
+
+        static IQueueClient queueClient;
+
+        public static async Task MainAsync(ServiceBusConnectionStringBuilder sbcsb)
+        {
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            queueClient = new QueueClient(sbcsb);
+
+            // Send messages.
+            await SendMessagesAsync(numberOfMessages);
+
+            await queueClient.CloseAsync();
+        }
+        
+
+        static async Task SendMessagesAsync(int numberOfMessages)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessages; i++)
+                {
+                    // create a new message to send to the queue.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
--- service-bus-queues-demo/Program.cs
+++ service-bus-queues-demo/Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using Microsoft.Azure.ServiceBus;
+using System;
 
-namespace service_bus_queues_demo
+namespace ServiceBusQueuesDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("------------------");
+            Console.WriteLine("1. CreateMessages");
+            Console.WriteLine("2. ReceiveMessages");
+            Console.WriteLine("------------------\n");
+
+            Console.Write("Enter choice: ");
+
+            var sbcs = GetConnectionStringBuilder(
+                endpoint: "sb://<endpoint-url>/",
+                queueName: "<queue-name>",
+                accessKeyName: "<shared-access-key-name>",
+                accessKey: "<shared-access-key>");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    CreateMessages.MainAsync(sbcs).GetAwaiter().GetResult();
+                    break;
+                case "2":
+                    ReceiveMessages.MainAsync(sbcs).GetAwaiter().GetResult();
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine("Done, press ANY KEY to exit.");
+            Console.ReadKey();
+        }
+
+        static ServiceBusConnectionStringBuilder GetConnectionStringBuilder(string endpoint, string queueName, string accessKeyName, string accessKey, TransportType transportType = TransportType.AmqpWebSockets)
+        {
+            return new ServiceBusConnectionStringBuilder(endpoint, queueName, accessKeyName, accessKey, transportType);
         }
     }
 }
