e19d5e7b7ffbfbe0c0462f74c9113f6982825494
Andy Mepham <andymeps@yahoo.com>
2017-12-15 12:45:21 +0000

    Add project files.

---
+++ service-bus-queues-demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace service_bus_queues_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
